6596afce06e68b22a91d986f37f6d143acfe4edb
Matvey <66329687+mavotronik@users.noreply.github.com>
2023-11-22 12:21:52 +0300

    Update run.sh

--- xmrig_miner_addon/run.sh
+++ xmrig_miner_addon/run.sh
@@ -6,7 +6,9 @@ MINING_POOL=$(bashio::config 'mining_pool')
 WALLET=$(bashio::config 'wallet')
 PASSWORD=$(bashio::config 'password')
 THREADS=$(bashio::config 'threads')
+MODE=$(bashio::config 'mode')
 
 cd xmrig/build
+echo $MODE
 touch config.json
 ./xmrig -o $MINING_POOL -u $WALLET -p $PASSWORD -t $THREADS
