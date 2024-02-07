0d087c7ce3e70be5ff4d38a47f0e942235483d00
Matvey <66329687+mavotronik@users.noreply.github.com>
2024-02-07 20:22:17 +0300

    Update run.sh

--- xmrig_miner_addon/run.sh
+++ xmrig_miner_addon/run.sh
@@ -5,10 +5,12 @@ echo "Hassio XMRig-Miner"
 MINING_POOL=$(bashio::config 'mining_pool')
 WALLET=$(bashio::config 'wallet')
 PASSWORD=$(bashio::config 'password')
+COIN=(bashio::config 'coin')
+ALGO=(bashio::config 'algo')
 THREADS=$(bashio::config 'threads')
 MODE=$(bashio::config 'mode')
 
 cd xmrig/build
 echo $MODE
 touch config.json
-./xmrig -o $MINING_POOL -u $WALLET -p $PASSWORD -t $THREADS
+./xmrig -o $MINING_POOL -u $WALLET -p $PASSWORD --coin $COIN -a $ALGO -t $THREADS --randomx-mode $MODE
